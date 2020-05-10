ecf0b22ae66082ac4dfe9af64639fed2dc9830ff
kayaramazan <kaya-ramazan@outlook.com>
2020-05-11 01:14:05 +0300

    socket integration

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,12 +1,72 @@
 app.controller('indexController', ['$scope','indexFactory',($scope,indexFactory ) =>{
-    indexFactory.connectSocket('http://localhost:3000',{
-        reconnectionAttemts:3,
-        reconnectionDelay:600
-    }).then((socket)=>
+    $scope.messages = [ ];
+    $scope.players = { };
+    $scope.init = () =>
     {
-        console.log('baglanti gerceklesti',socket.id);
-    }).catch((error) =>
+        let username = prompt("Enter a username");
+        if(username)
+        {
+            initSocket(username)
+        }else
+        return false
+    }
+    function initSocket(username)
     {
-        throw error;
-    })
+
+        indexFactory.connectSocket('http://localhost:3000',{
+            reconnectionAttemts:3,
+            reconnectionDelay:600
+        }).then((socket)=>
+        { 
+            console.log('burada');
+            socket.emit('newUser', { username });
+
+            socket.on('initPlayer',(players)=>
+            {
+                console.log(players);
+                $scope.players = players;
+                $scope.$apply();
+            })
+            socket.on('newUser',(data)=>
+            {
+                $scope.messages.push({
+                    type:{
+                        code:0,
+                        message:'katildi'
+                    },
+                    username:data.username
+                });
+                $scope.$apply();
+            })
+            socket.on('disUser',(data)=>
+            {
+                $scope.messages.push({
+                    type:{
+                        code:0,
+                        message:'cikis yapti'
+                    },//logout
+                    username:data.username
+                });
+                $scope.$apply();
+                console.log(data);
+            })
+            let animate = false;
+            $scope.onClickPlayer =  ($event) =>
+            {
+                if(!animate)
+                {
+                    animate=true;
+                    $('#'+socket.id).animate({ 'left':$event.offsetX, 'top':$event.offsetY }, () =>
+                    {
+                        animate=false;
+                    })
+                 }
+                console.log($event.offsetX, $event.offsetY)
+            };
+        }).catch((error) =>
+        {
+            throw error;
+        })
+    }
+    
 }])
--- src/socketApi.js
+++ src/socketApi.js
@@ -3,9 +3,34 @@ const io = socketio();
 
 const socketApi = {};
 socketApi.io=io;
+
+let users = { };
+
 io.on('connection', (socket) =>
 {
 console.log('connection succesful',socket.id);
+
+socket.on('disconnect', function(reason){
+
+    socket.broadcast.emit('disUser',users[socket.id]);
+    delete users[socket.id];
+});
+    socket.on('newUser',(data) =>
+    {
+        let defaultdata = {
+           id:socket.id,
+           position: {
+            x:0,
+            y:0
+           }
+        };
+
+        let userData = Object.assign(data, defaultdata);
+        users[socket.id]=userData; 
+        socket.broadcast.emit('newUser',userData);
+        socket.emit('initPlayer',users);
+    })
+
 });
 
 module.exports = socketApi;
